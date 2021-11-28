dc68f49278ecff36b93e953e4bf528e66bb825b4
25ramy <ramylinares@gmail.com>
2021-11-28 12:34:53 -0300

    added moda for Debe/Haber

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,52 @@
 <template>
   <div id="app">
-      <EditableTable v-model="items" :fields="fields"></EditableTable>
+
+      <EditableTable v-model="items" :fields="fields" :key="componentKey"></EditableTable>
+          <div>
+    <b-button v-b-modal.modal-1>Generar asiento</b-button>
+
+    <b-modal id="modal-1" title="BootstrapVue">
+    <p class="my-4">Cuenta Debe/Haber</p>
+        <b-form @submit="onSubmit" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Cuenta Debe: "
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.ctaDebe"
+          placeholder="Ingrese el nombre de la cuenta Debe"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Cuenta Haber:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.ctaHaber"
+          placeholder="Ingrese el nombre de la cuenta del Haber"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+<b-form-group id="input-group-2" label="Cantidad:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          type="number"
+          v-model="form.cantidad"
+          placeholder="Ingrese la cantidad de las cuentas"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+        <b-form-datepicker v-model="form.fecha"></b-form-datepicker>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      
+    </b-form>
+
+    </b-modal>  
+    </div>
       <!-- <b-button v-on:click="libroMayor">Generar libro Mayor</b-button> -->
         <b-button type="button" @click="libroMayor">Generar libro Mayor</b-button>
   <component
@@ -72,9 +118,26 @@ console.log(this.items[i].descripcion);
  }
 this.comp2.push(Comp)
 },
+onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+        let debe={cantidad: this.form.cantidad, descripcion: this.form.ctaDebe, tipo: 'Debe', fecha: this.form.fecha}
+        let haber={cantidad: this.form.cantidad, descripcion: this.form.ctaHaber, tipo: 'Haber', fecha: this.form.fecha}
+        this.items.push(debe)
+        this.items.push(haber) 
+        this.componentKey += 1
+      },
   },
   data() {
     return {
+      componentKey: 0,
+      form: {
+          ctaDebe: '',
+          ctaHaber: '',
+          cantidad: 0,
+          fecha: ''
+        },
+        show: true,
       comp2: [Comp],
       fields: [
         { key: "selectRow", label: "" },
@@ -87,8 +150,8 @@ this.comp2.push(Comp)
        items: [
           { cantidad: 100, descripcion: 'Caja', tipo: 'Debe', fecha: '2021-05-20' },
           { cantidad: 150, descripcion: 'Deudores Varios', tipo: 'Debe', fecha: '2021-05-20' },
-          { cantidad: 100, descripcion: 'Caja', tipo: 'Haber', fecha: '2021-05-20' },
-          { cantidad: 150, descripcion: 'Bco Cta Cte', tipo: 'Haber', fecha: '2021-05-20' }
+          { cantidad: 150, descripcion: 'Caja', tipo: 'Haber', fecha: '2021-05-20' },
+          { cantidad: 100, descripcion: 'Bco Cta Cte', tipo: 'Haber', fecha: '2021-05-20' }
         ]
     };
   }
